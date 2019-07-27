c95567262f61b8ddb766399eb6ef07fbb839d287
compileVikram <vikramvkadam91@gmail.com>
2019-07-27 15:44:25 +0530

    new file

---
+++ src/A_Basic/b_Demo02.java
@@ -0,0 +1,25 @@
+package A_Basic;
+
+/**
+ * @author Vikram Vijay kadam
+ *
+ */
+public class b_Demo02 
+{
+
+	
+	public static void main(String[] args) 
+	{
+     
+		
+		System.out.println("Hi, Welcome all"+ " to CORE-JAVA class");
+		System.out.println("Sum of 4 & 5 is "+4+5 );
+		System.out.println(4+5+" is the sum of 4 & 5");
+		System.out.println();
+		System.out.print("Hello");
+		System.out.println(" Bangalore & Mysore");
+		System.out.println("Sum of 4 and 5 is "+(4+5));
+	}
+
+}
+ 
